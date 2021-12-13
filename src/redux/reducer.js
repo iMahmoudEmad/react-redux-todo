f26a9647b8ed3ac7670fd07ed59503f964588c71
Mahmoud Emad Eldin <mahmoud.emad@areebgroup.com>
2021-12-13 21:02:52 +0200

    Added Provider & store to index file

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,7 @@
+import { TODOS_CONSTANS } from './constans';
 import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from './types';
 
-export const reducer = (state, action) => {
+export const reducer = (state = TODOS_CONSTANS, action) => {
 	switch (action.type) {
 		case ADD_TODO:
 			break;
