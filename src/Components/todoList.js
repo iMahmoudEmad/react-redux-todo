a2d300f1b88a3e7e1348025c5c722293cdb4cce4
Mahmoud Emad Eldin <mahmoud.emad@areebgroup.com>
2021-12-13 21:11:25 +0200

    Added global todos constant & useSelector into todoList component

--- src/Components/todoList.js
+++ src/Components/todoList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import TodoItem from './todoItem';
 
 const TodoList = () => {
+	const todos = useSelector((state) => state);
 	return (
 		<div className='formInput'>
-			<TodoItem />
-			<TodoItem />
-			<TodoItem />
+			{todos?.map((todo) => (
+				<TodoItem key={todo?.id} name={todo?.name} status={todo?.status} />
+			))}
 		</div>
 	);
 };
