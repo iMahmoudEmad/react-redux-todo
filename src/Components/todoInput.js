d7dbc10b3e6ca88f07701a4e9f7e7e713a0e4e7b
Mahmoud Emad Eldin <mahmoud.emad@areebgroup.com>
2021-12-13 21:38:59 +0200

    Added addTodo dispatch & install uuid

--- src/Components/todoInput.js
+++ src/Components/todoInput.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/action';
+import { v4 as uuidv4 } from 'uuid';
 
 const TodoInput = () => {
+	const [todo, setTodo] = useState('');
+	const dispatch = useDispatch();
+
 	return (
 		<div className='formInput'>
-			<input type='text' placeholder='Enter Todo Name...' />
-			<button>Add</button>
+			<input
+				type='text'
+				placeholder='Enter Todo Name...'
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
+			/>
+			<button onClick={() => dispatch(addTodo({ id: uuidv4(), name: todo }))}>
+				Add
+			</button>
 		</div>
 	);
 };
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,13 +2,16 @@ import { TODOS_CONSTANT } from './constans';
 import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from './types';
 
 export const reducer = (todos = TODOS_CONSTANT, { type, data }) => {
+	let updatedTodos;
 	switch (type) {
 		case ADD_TODO:
-			break;
+			updatedTodos = [...todos];
+			updatedTodos.push(data);
+			return updatedTodos;
 		case DELETE_TODO:
-			let filteredTodos = [...todos];
-			filteredTodos = filteredTodos.filter((todo) => todo.id !== data);
-			return filteredTodos;
+			updatedTodos = [...todos];
+			updatedTodos = updatedTodos.filter((todo) => todo.id !== data);
+			return updatedTodos;
 		case UPDATE_TODO:
 			break;
 		default:
